cd9261d0096b382c6a0714c17a3824287f8f2920
Sudhanshu060924 <Sudhanshusingh0624@gmail.com>
2026-01-27 23:32:17 +0530

    add profile page and add edit message part

--- frontend/src/components/Messagearea.jsx
+++ frontend/src/components/Messagearea.jsx
@@ -15,7 +15,7 @@ function Messagearea() {
             onClick={() => dispatch(setSelectedUser(null))}
             className="cursor-pointer"
           >
-            <IoArrowBackOutline className="text-2xl ml-4 " />
+            <IoArrowBackOutline className="text-2xl ml-6 " />
           </div>
           <div
             // onClick={() => navigate("/profile")}
